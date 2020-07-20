6cb529a5c390c3592ef1f716d90e7ba70485c593
Kathleen Yoannon <k.yoannon@gmail.com>
2020-07-19 18:48:45 -0700

    Update submit-data.js
    test code for submit

--- routes/submit-data.js
+++ routes/submit-data.js
@@ -1,12 +1,33 @@
-// Requiring our models
-const db = require("../models");
-const express = require('express');
-const router  = express.Router();
-const fuel_controller = require("../controllers/fuel_controller")
-const isAuthenticated = require("../config/middleware/isAuthenticated")
 
-module.exports = function(app) {
-// POST route for saving a new post
-app.post('/submit-data', fuel_controller.createPost);
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = new Sequelize("sqlite::memory:");
 
-};
+//need to log new current fuel coming in from the employee/user
+const Fuel = sequelize.define("fuel", {
+  fueltype: DataTypes.TEXT,
+  vehicle: DataTypes.INTEGER,
+  gallons: DataTypes.INTEGER
+});
+
+(async () => {
+    await sequelize.sync({ force: true });
+    // Code here
+
+
+const newFuel = Fuel.build({ newFuel: "gallons" });
+console.log(gallons instanceof Fuel); // true
+console.log(newFuel.Fuel); // "employees new fuel log in gallons"
+
+await newFuel.save();
+console.log('newFuel was saved to the database!');
+
+const newFuel = await Fuel.create({ newFuel: "gallons"});
+console.log(newFuel.toJSON()); // This is good!
+console.log(JSON.stringify(newFuel, null, 4));
+
+await newFuel.reload();
+console.log(newFuel.Fuel);
+})
+(
+
+);
