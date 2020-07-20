466af2a530f55e4eb3e5c80899065c2291ea58f6
Kathleen Yoannon <k.yoannon@gmail.com>
2020-07-19 19:21:15 -0700

    Update submit.js
    front end submit data.js side

--- public/assets/js/submit.js
+++ public/assets/js/submit.js
@@ -1,14 +1,33 @@
-$(document).ready(function () {
 
-    
-     $("#addBtn").on("click", event => {
-         event.preventDefault();
-         window.location.assign("submit-data");
-     })
- 
-     $("#viewBtn").on("click", event => {
-         event.preventDefault();
-         window.location.assign("view-data");
-     })
- 
- });
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = new Sequelize("sqlite::memory:");
+
+//need to log new current fuel coming in from the employee/user
+const Fuel = sequelize.define("fuel", {
+  fueltype: DataTypes.TEXT,
+  vehicle: DataTypes.INTEGER,
+  gallons: DataTypes.INTEGER
+});
+
+(async () => {
+    await sequelize.sync({ force: true });
+    // Code here
+
+
+const newFuel = Fuel.build({ newFuel: "gallons" });
+console.log(gallons instanceof Fuel); // true
+console.log(newFuel.Fuel); // "employees new fuel log in gallons"
+
+await newFuel.save();
+console.log('newFuel was saved to the database!');
+
+const newFuel = await Fuel.create({ newFuel: "gallons"});
+console.log(newFuel.toJSON()); // This is good!
+console.log(JSON.stringify(newFuel, null, 4));
+
+await newFuel.reload();
+console.log(newFuel.Fuel);
+})
+(
+
+);
