0aa367c82138759e64e0c595bbcfed633cf3267c
Kathleen Yoannon <k.yoannon@gmail.com>
2020-07-25 16:56:39 -0700

    added css for logo and footer, cant figure out the sytax for the @media

---
+++ public/assets/js/signin.js
@@ -0,0 +1,13 @@
+    // Adding the show password option
+    $(document).ready(function () {
+    const togglePassword = document.getElementById('togglePassword');
+    const showOrHidePassword = () => {
+      const password = document.getElementById('password');
+      if (password.type === 'password') {
+        password.type = 'text';
+      } else {
+        password.type = 'password';
+      }
+    };    
+    togglePassword.addEventListener('change', showOrHidePassword);
+});
